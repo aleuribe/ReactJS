c297b81b41ebb9e76d17ef72af2c1cdf5cc725e9
Alejandro Uribe <uribe.alejandro30@gmail.com>
2021-08-30 17:52:40 -0300

    DAgregando el menu con bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
-const Titulo = ({firstName, lastName}) => {
-  console.log(firstName)
-
-  return <h1>Hola {firstName} {lastName}</h1>
-}
-
+import NavBar from './components/NavBar/NavBar'
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <Titulo firstName="Ale" lastName="Uribe" />
-        <Titulo firstName="Key" />
-
+        <NavBar />
         <img src={logo} className="App-logo" alt="logo" />
       </header>
 
     <h1>Bienvenido a la tienda NFStore.com</h1>
-    <p>En construccion</p>
+    <p>Tienda online de Tokens No Fungibles - En construccion</p>
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,58 @@
+import 'bootstrap/dist/css/bootstrap.css'
+
+const NavBar = () => {
+  
+    return (
+
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">
+      <img src="logo192.png" width="30" height="30" class="d-inline-block align-top" alt="" />
+      NFStore.com
+      </a>
+
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Inicio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Mas Vendidos</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Categorias</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Informacion</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <form className="row row-cols-sm-auto g-3 align-items-center">
+          <div className="col-12">
+            <label className="visually-hidden" for="inlineFormInputGroupUsername">Username</label>
+            <div className="input-group">
+              <div className="input-group-text">@</div>
+                <input type="text" className="form-control" id="inlineFormInputGroupUsername" placeholder="Nombre de Usuario"/>
+              
+            </div>
+          </div>
+          
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary">Iniciar sesion</button>
+          </div>
+
+        </form>
+
+        </div>
+
+    </nav>
+
+  )
+}
+
+  export default NavBar
