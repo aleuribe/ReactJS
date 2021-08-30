76a210b4b5725c4a75282d824c53a37f650deabd
Alejandro Uribe <alejandro@MacBook-Air-de-Alejandro.local>
2021-08-30 14:29:52 -0300

    last comments from NFT

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Titulo = ({firstName, lastName}) => {
+  console.log(firstName)
+
+  return <h1>Hola {firstName} {lastName}</h1>
+}
+
+
+const App = () => {
   return (
     <div className="App">
+      <header className="App-header">
+        <Titulo firstName="Ale" lastName="Uribe" />
+        <Titulo firstName="Key" />
+
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+
     <h1>Bienvenido a la tienda NFStore.com</h1>
     <p>En construccion</p>
     </div>
